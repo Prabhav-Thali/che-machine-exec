f74a13cfcf3f7047839ffb32c2289362caeffa0d
Prabhav Thali <Prabhav.Thali1@ibm.com>
2021-05-04 13:05:58 +0530

    Adds multiarch build

---
+++ .travis/unit_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+CGO_ENABLED=0 GOOS=linux go build -mod=vendor -a -ldflags '-w -s' -a -installsuffix cgo -o che-machine-exec .
+export CHE_WORKSPACE_ID=test_id
+go test ./... -test.v
+docker build -f build/dockerfiles/Dockerfile -t quay.io/prabhav/che-machine-exec:pr-check-${TRAVIS_CPU_ARCH} . 
