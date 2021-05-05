60e72776489472279b241782f9ae822d197653f3
Prabhav Thali <Prabhav.Thali1@ibm.com>
2021-05-05 11:28:23 +0530

    Update publish_multiarch.sh

--- .travis/publish_multiarch.sh
+++ .travis/publish_multiarch.sh
@@ -11,10 +11,10 @@ AMEND+=" --amend quay.io/prabhav/che-machine-exec:$TAG-s390x";
 echo $AMEND
 echo $TAG
 
-docker manifest create quay.io/prabhav/che-machine-exec:"$TAG" "$AMEND"
-docker manifest push quay.io/prabhav/che-machine-exec:"$TAG"
+docker manifest create quay.io/prabhav/che-machine-exec:"${TAG}" "${AMEND}"
+docker manifest push quay.io/prabhav/che-machine-exec:"${TAG}"
 
 if [[ "$TAG" == "nightly" ]]; then
-    docker manifest create quay.io/prabhav/che-machine-exec:"${SHORT_SHA}" "$AMEND"
+    docker manifest create quay.io/prabhav/che-machine-exec:"${SHORT_SHA}" "${AMEND}"
     docker manifest push quay.io/prabhav/che-machine-exec:"${SHORT_SHA}"
 fi
